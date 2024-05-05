4a3906980f75f2b76940cf7e7b6883920c26407e
Malik <timurkaev_malik@icloud.com>
2024-05-05 17:56:51 +0300

    CoreDataManager

--- Tracker/CoreData/CoreDataManager.swift
+++ Tracker/CoreData/CoreDataManager.swift
@@ -5,4 +5,21 @@
 //  Created by Malik Timurkaev on 05.05.2024.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+final class CoreDataManager: NSObject {
+    
+    public static let shared = CoreDataManager()
+    
+    private override init(){}
+    
+    
+    private var appDelegate: AppDelegate {
+        UIApplication.shared.delegate as! AppDelegate
+    }
+    
+    private var context: NSManagedObjectContext {
+        appDelegate.persistentContainer.viewContext
+    }
+}
